01b89e068637da7eaf3937a0d5410b69dabae4e5
FloLys <4440@holbertonschool.com>
2022-02-21 18:19:17 -0800

    7 upload

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: array
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i] < 8; i++)
+	{
+		for (j = 0; a[j] < 8; j++)
+			_putchar(a[i][j]);
+		
+		_putchar('\n');
+}
