7552d4315c63fdc716b11dde06835c0a43f83993
FloLys <4440@holbertonschool.com>
2022-02-23 09:55:04 -0800

    directory and 0 upload

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string with new line
+ * @s: string
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	
+	else
+		_putchar(s);
+		_puts_recursion(s + 1);
+}
