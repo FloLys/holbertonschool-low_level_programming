0f205573bfdcff4beceb87841ec9ecd56918236d
FloLys <4440@holbertonschool.com>
2022-02-23 10:58:55 -0800

    2 tested

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: (n)
+*/
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s == '\0')
+	{
+		return(0);
+	}
+
+		else
+		{
+			n = _strlen_recursion(s + 1) + 1;
+		}
+
+	return(n);
+}
