24198da5ca948f0199a58a4333e3850441dce711
FloLys <4440@holbertonschool.com>
2022-02-23 19:00:58 -0800

    6 update test

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,11 +7,7 @@
 */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-
-	else
-		return (prime(2, n));
+	return (prime(n, 1));
 }
 
 /**
@@ -22,15 +18,18 @@ int is_prime_number(int n)
 */
 int prime(int n, int x)
 {
-	if (n % x == 0)
+	if (n <= 1)
 		return (0);
 
-	else if (x >= n / 2)
+	else if (x == n)
 		return (1);
 
-	else if (n % x != 0)
-		return (prime(x + 1, n));
-
 	else
-		return (-1);
+	{
+		if (n % x == 0 && x != 1 && x != n)
+			return (0);
+	}
+
+	return (prime(n, x + 1));
+	return (0);
 }
