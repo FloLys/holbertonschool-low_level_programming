b1c8416f57e35a20dcb49a8703d694699ea8d469
FloLys <4440@holbertonschool.com>
2022-02-25 12:02:34 -0800

    4 tested

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,6 +16,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
+			/*could be if (isdigit(argv[i][j]) == 0)*/
 			if (!(argv[i][j] >= '0' &&  argv[i][j] <= '9'))
 			{
 				printf("Error\n");
@@ -23,7 +24,7 @@ int main(int argc, char *argv[])
 			}
 		}
 
-	sum += atoi(argv[j]);
+	sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
