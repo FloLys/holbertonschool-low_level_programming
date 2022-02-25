7c3d5c81c1cdce3e5a418e99d43073d48a8092a2
FloLys <4440@holbertonschool.com>
2022-02-25 09:16:10 -0800

    0 and 1 update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,6 +5,7 @@
  * main - program prints its name
  * @argc: counter for arguments
  * @argv: array of strings of arguments
+ * Return: 0
 */
 int main(int argc __attribute__((unused)), char *argv[])
 {
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,10 +5,11 @@
  * main - print the number of arguments passed into it
  * @argc: counter for arguments
  * @argv: array of strings of arguments
+ * Return: 0
 */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
