e9b305dc0e6a02ac9c1a38d8a9752d42ce1524f7
FloLys <4440@holbertonschool.com>
2022-02-25 09:18:52 -0800

    2 update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,17 @@
  * main - print all arguments it recieves
  * @argc: counter for arguments
  * @argv: array of strings of arguments
+ * Return: 0
 */
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	{
+		printf("%s", argv[i]);
+		printf("\n");
+	}
 
 	return (0);
 }
