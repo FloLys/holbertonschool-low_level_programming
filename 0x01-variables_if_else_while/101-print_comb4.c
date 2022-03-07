afa3b60ffa9cb39e4c574820575e92acfbe8faa8
FloLys <4440@holbertonschool.com>
2022-03-07 05:44:52 -0800

    101 update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ int main(void)
 	int n1, n2, n3;
 
 	for (n1 = 0; n1 <= 9; n1++)
-	{	
+	{
 		for (n2 = n1 + 1; n2 <= 9; n2++)
 		{
 			for (n3 = n2 + 1; n3 <= 9; n3++)
@@ -22,22 +22,6 @@ int main(void)
 				putchar(n3 + 48);
 				putchar(',');
 				putchar(' ');
-				/*
-				if (x == 0)
-				{
-					putchar(n1);
-					putchar(n2);
-					x = 1;
-				}
-
-				else
-				{
-					putchar(',');
-					putchar(' ');
-
-					putchar(n1);
-					putchar(n2);
-				}*/
 			}
 		}
 	}
