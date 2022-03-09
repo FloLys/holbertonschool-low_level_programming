7b31fd11cfd0ab5f5a08f59429df6dd9a564f6f9
FloLys <4440@holbertonschool.com>
2022-03-09 07:04:22 -0800

    0 and .h updated

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* array_iterator - executes a fctn given as a parameter on each element
+* of an array
+* @array: 
+* @size: 
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	f(name);
+}
