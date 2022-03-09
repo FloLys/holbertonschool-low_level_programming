8bfc16248d433853d2ce45594b2ae1c455242cae
FloLys <4440@holbertonschool.com>
2022-03-08 17:56:13 -0800

    dog update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+* struct dog - stores data of class dog
+* @name: name
+* @age: age
+* @owner: owner
+*/
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif
