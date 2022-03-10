c82d6faeb0df5d80ee6f243d5cdffd8786a430fa
FloLys <4440@holbertonschool.com>
2022-03-09 18:20:19 -0800

    calc corrections

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (!(argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*'
-		|| argv[2][0] == '/' || argv[2][0] == '%') && argv[2][1] == '\0')
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+		&& argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && argv[3] == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == 0)
 	{
 		printf("Error\n");
 		exit(100);
