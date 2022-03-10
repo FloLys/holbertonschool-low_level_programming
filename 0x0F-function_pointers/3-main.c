39a01dd11690afed023866673d91459e7cd14207
FloLys <4440@holbertonschool.com>
2022-03-09 17:46:45 -0800

    calc update?

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,35 +1,35 @@
 #include "3-calc.h"
 
 /**
-* main - adds positive numbers
+* main - called function to do the operations
 * @argc: argument 1
 * @argv: argument 2
 * Return: 0
 */
 int main(int argc, char *argv[])
 {
-int a = 0, b = 0;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        exit (98);
-    }
+	if (!(argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*'
+		|| argv[2][0] == '/' || argv[2]0[] == '%') && argv[2][1] == '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if (!(argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*' || argv[2][0] == '/' || argv[2]0[] == '%') && argv[2][1] == '\0' )
-    {
-        printf("Error\n");
-        exit (99);
-    }
+	if ((argv[2] == '/' || argv[2] == '%') && argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    if ((argv[2] == '/' || argv[2] == '%') && argv[3] == 0)
-    {
-        printf("Error\n");
-        exit (100);
-    }
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
-
-    return (0);
+	printf("%d\n", ((*get_op_func(argv[2]))(a, b)));
+	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
-* op_add - adds positive numbers
+* op_add - addition
 * @a: number 1
 * @b: number 2
 * Return: sum
@@ -9,4 +9,48 @@
 int op_add(int a, int b)
 {
 	return (a + b);
+}
+
+/**
+* op_sub - substract
+* @a: number 1
+* @b: number 2
+* Return: sub
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multiplicate
+* @a: number 1
+* @b: number 2
+* Return: mul
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - divide
+* @a: number 1
+* @b: number 2
+* Return: div
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - module
+* @a: number 1
+* @b: number 2
+* Return: mod
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
