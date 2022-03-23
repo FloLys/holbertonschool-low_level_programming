2a21a3a218a017141c900de67471fde412c626be
FloLys <4440@holbertonschool.com>
2022-03-22 19:17:07 -0700

    6 upload

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* pop_listint - deletes head node
+* @head: head with the list
+* Return: head node's data
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *aux;
+	int data;
+
+	if (head == NULL)
+		return (0);
+
+	else
+	{
+		aux = *head;
+		data = (*head)->n;
+		*head = (*head)->next;
+		free(aux);
+	}
+
+	return (data);
+}
