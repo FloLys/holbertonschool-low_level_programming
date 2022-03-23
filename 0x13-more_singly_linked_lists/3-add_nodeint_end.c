1924cc7bf64eaaa9d765621306ae6328f63105ba
FloLys <4440@holbertonschool.com>
2022-03-22 18:48:45 -0700

    3 and 5 update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,22 +9,23 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *last_node = *head;
+	listint_t *last_node;
 
 	if (new_node == NULL)
 		return (NULL);
 
 	(*new_node).n = n;
+	(*new_node).next = NULL;
 
 	if (*head == NULL)
 		*head = new_node;
 
 	else
 	{
+		last_node = *head;
 		while (last_node->next != NULL)
 			last_node = last_node->next;
 
-		new_node->next = last_node->next;
 		last_node->next = new_node;
 	}
 	return (new_node);
