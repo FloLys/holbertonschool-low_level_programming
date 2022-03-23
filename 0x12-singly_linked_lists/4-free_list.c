cce9746bc462b51bf34167242a48ddb425512cc2
FloLys <4440@holbertonschool.com>
2022-03-22 17:26:03 -0700

    4 upload tested

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+* free_list - frees a list_t list
+* @head: head with the list
+*/
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
