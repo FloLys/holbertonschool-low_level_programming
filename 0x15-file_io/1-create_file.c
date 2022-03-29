aad70678cd6e589ed7b2e03e0de3c118b7830086
FloLys <4440@holbertonschool.com>
2022-03-29 10:50:26 -0700

    1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int i, wr, fd;
 
-	if (!filename || *filename == '\0')
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT, 600 | O_TRUNC | O_RDWR);
+	fd = open(filename, O_CREAT | O_RDONLY | O_WRONLY, 0600);
 
 	if (fd == -1)
 		return (-1);
