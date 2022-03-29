d583fef04f162f24d5b3cc99a59201fdabbbaf5c
FloLys <4440@holbertonschool.com>
2022-03-29 10:45:57 -0700

    1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,18 +8,24 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int i, wr, fd = open(filename, O_CREAT, 600 | O_TRUNC | O_RDWR);
+	int i, wr, fd;
 
 	if (!filename || *filename == '\0')
 		return (-1);
 
+	fd = open(filename, O_CREAT, 600 | O_TRUNC | O_RDWR);
+
 	if (fd == -1)
 		return (-1);
 
-	for (i = 0; text_content[i] != '\0'; i++)
+	if (text_content == NULL)
+		text_content = "";
+
+	for (i = 0; text_content[i]; i++)
 
 	wr = write(fd, text_content, i);
-	if (wr == -1)
+
+	if (wr != i)
 		return (-1);
 
 	close(fd);
