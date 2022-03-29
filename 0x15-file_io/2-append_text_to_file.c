bd078ed79687fe0747aa94ac8ebb20e4e331d112
FloLys <4440@holbertonschool.com>
2022-03-29 12:09:36 -0700

    2 update corrected case

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,9 +18,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 
-	while (text_content[i])
+	while (text_content[i] != '\0')
 		i++;
 
 	wr = write(fd, text_content, i);
