a6be9d62119cdd848690100f357a9db7f284bde2
FloLys <4440@holbertonschool.com>
2022-03-28 19:47:27 -0700

    directory and 0 upload

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and prints it to the posix sdout
+* @filename: filename string
+* @letters: number of letters to read and print
+* Return: actual number of letters it could read and print
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	char *buffer = malloc(sizeof(char) * letters + 1);
+
+	if (!filename || *filename == '\0')
+		return (0);
+	
+	if (fd == -1)
+		return (0);
+
+	if (buffer == NULL)
+		return (0);
+	
+	read(fd, buffer, letters);
+	buffer[letters] = '\0';
+	fd = write(STDOUT_FILENO, buffer, letters);
+	close (fd);
+
+	return (fd);
+}
