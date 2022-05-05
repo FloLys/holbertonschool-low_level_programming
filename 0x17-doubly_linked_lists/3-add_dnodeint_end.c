1f08e2a5f373e84d5f1401e8ecc7bdfd94278d10
FloLys <4440@holbertonschool.com>
2022-05-05 11:27:13 -0700

    3 corrected

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,17 +14,22 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	(*new_node).n = n;
-	(*new_node).next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (*head == NULL)
+	{
+		new_node->prev = NULL;
 		*head = new_node;
+	}
 	else
 	{
 		last_node = *head;
 		while (last_node->next != NULL)
 			last_node = last_node->next;
+
 		last_node->next = new_node;
+		new_node->prev = last_node;
 	}
 	return (new_node);
 }
