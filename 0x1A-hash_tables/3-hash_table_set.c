6d0bd0b3d920d86ae05a748e17d79f503bfb7291
FloLys <4440@holbertonschool.com>
2022-05-27 16:45:41 -0700

    3 case correct

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new_element = NULL, *current_elem = NULL;
 
-	if (value == NULL)
+	if (!ht || !key)
 		return (0);
 
 	current_elem = ht->array[index];
